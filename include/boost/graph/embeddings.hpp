22d0f24f3456a30d52c6d5c2ff329e3f57403c33
Hermann-SW <hermann@stamm-wilbrandt.de>
2024-10-21 11:09:30 +0200

    Pass graph as reference for simple functions as well

--- include/boost/graph/embeddings.hpp
+++ include/boost/graph/embeddings.hpp
@@ -16,26 +16,26 @@ namespace boost
 {
 
 template< class graph >
-bool is_embedding(graph g, std::size_t n_faces, std::size_t genus,
+bool is_embedding(graph& g, std::size_t n_faces, std::size_t genus,
                   std::size_t n_ccs=1)
 {
     return n_faces + num_vertices(g) - num_edges(g) == 1 + n_ccs - 2 * genus;
 }
 
 template< class graph >
-bool is_planar_embedding(graph g, std::size_t n_faces, std::size_t n_ccs = 1)
+bool is_planar_embedding(graph& g, std::size_t n_faces, std::size_t n_ccs = 1)
 {
     return is_embedding(g, n_faces, 0, n_ccs);
 }
 
 template< class graph >
-bool is_torodial_embedding(graph g, std::size_t n_faces, std::size_t n_ccs = 1)
+bool is_torodial_embedding(graph& g, std::size_t n_faces, std::size_t n_ccs = 1)
 {
     return is_embedding(g, n_faces, 1, n_ccs);
 }
 
 template< typename graph >
-std::size_t genus(graph g, std::size_t n_faces, std::size_t n_ccs = 1)
+std::size_t genus(graph& g, std::size_t n_faces, std::size_t n_ccs = 1)
 {
     std::size_t u = num_edges(g) + 1 + n_ccs - n_faces - num_vertices(g);
     BOOST_ASSERT(u % 2 == 0);
